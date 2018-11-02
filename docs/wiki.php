78859de287073b3cc90f63a0ef864a01aea19aca
Philip Dhingra <philipkd@gmail.com>
2018-11-02 14:15:08 -0700

    generates wiki links, but misses edge cases

--- docs/wiki.php
+++ docs/wiki.php
@@ -30,10 +30,12 @@ function MyMarkdown2($contents) {
 
 if ($_GET['note'])
 	$note = unslug($_GET['note']);
+elseif ($argv[1])
+	$note = $argv[1];
 else
 	$note = "Home";
 
-$contents = file_get_contents('../content/Wiki/' . $note . '.txt');
+$contents = file_get_contents(dirname(__FILE__) . '/../content/Wiki/' . $note . '.txt');
 
 $title = preg_replace('/(.*){0,1}\/(.*)/','\2',$note);
 
--- scripts/build_wiki.php
+++ scripts/build_wiki.php
@@ -1,5 +1,27 @@
 <?php
 
+# $cmd = `php docs/wiki.php 
+
+function slug($str) {
+	return preg_replace('/ /','_',$str);
+}
+
+function unslug($str) {
+	return preg_replace('/_/',' ',$str);
+}
+
+$folder = './content/Wiki';
+if ($folder_handle = opendir($folder)) {
+	while (false !== ($file = readdir($folder_handle))) {
+		if (preg_match('/\.txt$/',$file)) {
+			$justfile = addslashes(slug($file));
+			$cmd = "php docs/wiki.php " . str_replace(".txt","",$justfile) . " > \"./docs/wiki/" . str_replace(".txt",".html",$justfile) . "\"";
+			print "$cmd\n";
+			`$cmd`;
+		}
+	}
+}
+
 
 
 ?>
