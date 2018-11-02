0e5dc37843a038107d99cf718a3ffd80cf3b9cb8
Philip Dhingra <philipkd@gmail.com>
2018-11-02 09:47:06 -0700

    built content and made build script, plus fixed regex on .htaccess

--- scripts/build_db.php
+++ scripts/build_db.php
@@ -5,9 +5,18 @@
 ?> 
 
 <?php
+	include_once "docs/markdown.php";
 
-	$GLOBALS['files_dir'] = "./docs/content/Files";
-	$GLOBALS['logs_dir'] = "./docs/content/Logs";
+	if (preg_match("/^local./", $_SERVER['HTTP_HOST']))
+		$GLOBALS['local_access'] = true;
+
+    $GLOBALS['tag_route'] = $_GET['tag'];
+
+    if ($argv[1])
+    	$GLOBALS['tag_route'] = $argv[1];
+
+	$GLOBALS['files_dir'] = dirname(__FILE__) . "/../docs/content/Files";
+	$GLOBALS['logs_dir'] = dirname(__FILE__) . "/../docs/content/Logs";
 
 	$GLOBALS['essays'] = array();
 
@@ -16,18 +25,15 @@
 
 	$GLOBALS['tag_to_title'] = array();
 
+	# $GLOBALS['index'] = json_decode(file_get_contents("./content/index.json"));
+
 	process_notes();
 	process_logs();
 
 	function print_nav_tags($tags) {
 	    foreach ($tags as $tag) {
-
-	    	$cmd = "php docs/db.php $tag > docs/db/$tag.html";
-
-	    	print "$cmd\n";
-
-	    	# $count = count($GLOBALS['tag_to_essays'][$tag]);
-		    # echo "<img src=\"/tag.png\"> <a href=\"$tag\">" . titleify($tag) . "</a> ($count)<br/>\n";
+	    	$count = count($GLOBALS['tag_to_essays'][$tag]);
+		    echo "<img src=\"/tag.png\"> <a href=\"$tag\">" . titleify($tag) . "</a> ($count)<br/>\n";
 		}
 	}
 
@@ -59,6 +65,12 @@
 	    print "<br/>Tags<br/>";
 	    	print_nav_tags($main_tags);
 
+		/* if ($GLOBALS['local_access']) {
+		    print "<br/>Special Tags<br/>";
+		    print_nav_tags($special_tags);
+		} */
+
+	    echo "</div>";
 	}
 
 	function hash_tags_and_title($tags,$title) {
@@ -147,6 +159,151 @@
 	}
 
 
+	function print_tag($tag) {
+ 		$essays = $GLOBALS['tag_to_essays'][$tag];
+ 		natsort($essays);
+
+		echo "<h2>" . format_tag($tag) . "</h2>\n";
+
+		$idea = ltrim($tag, "_");
+
+		if ($idea_data = $GLOBALS['index']->ideas->$idea) {
+
+			echo "<div class='tag-description'>";
+
+			if ($subtitle = $idea_data->subtitle)
+				echo "<p><b>" . $subtitle . "</b></p>";
+
+			if ($description = $idea_data->description) {
+				echo MyMarkdown($description);
+			}			
+
+			echo "</div>";
+			echo "<h3>Entries</h3>";
+		}
+
+
+
+		// $i = 0;
+ 		foreach ($essays as $essay) {
+			print_essay($essay);
+
+			// if($i < count($essays) - 1)
+			// 	hr_tag();
+			// $i++;
+ 		}
+	}
+
+	function print_book() {
+
+		echo <<<EOT
+<h2>Philosophistry, the Book</h2>
+<div class="note-body">
+<center><a href="https://www.amazon.com/gp/product/1530775183/?tag=philosophistr-20">Available now in paperback and on Kindle<!-- <br/><br/><img src="books/philosophistry/cover-thumb.png" alt="Philosophistry book cover" /> --></a></center>
+</div>
+EOT;
+
+	}
+
+	function print_ideas_from_array($ideas) {
+		foreach ($ideas as $idea) {
+					$data = $GLOBALS['index']->ideas->$idea;
+
+					echo "<b><a href='?tag=_$idea'>" . $data->title . "</b> — " . $data->subtitle . "</a><br/><br/>"; 
+				}
+	}
+
+	function print_ideas() {
+
+		echo '<div class="note-body">';
+
+		print_ideas_from_array($GLOBALS['index']->live);
+
+		if ($GLOBALS['local_access']) {
+
+			echo "<br/>";
+
+			$unlive_ideas = array_diff(array_keys((array) $GLOBALS['index']->ideas), $GLOBALS['index']->live);
+
+			print_ideas_from_array($unlive_ideas);
+		}	
+
+
+		echo '</center></div>';
+	}
+
+	function print_top_essays() {
+
+		echo <<<EOT
+<h2>Top 9 Essays</h2>
+
+<ol>
+<li><a href="https://posts.philipkd.com/wobbly-tables-and-the-problem-with-futurism-934468d2308">Wobbly Tables and the Problem with Futurism</a></li>
+<li><a href="http://philosophistry.com/archives/2010/07/todays-what-if-what-if-knights-were-just-gangstas.html">What if Knights were just Gangstas?</a></li>
+<li><a href="https://www.reddit.com/r/writing/comments/1eje3d/random_thought_writing_is_attachment/">Writing is Attachment</a></li>
+<li><a href="https://www.reddit.com/r/StonerPhilosophy/comments/19bfoc/drawing_a_line_through_multiverses/">Drawing a Line Through Multiverses</a></li>
+<li><a href="https://www.reddit.com/r/politics/comments/c010j/southerners_like_to_believe_they_were_fighting/">Southerners like to believe they were fighting for their freedoms, when really they were fighting for Plantation owners' freedom to own slaves. You see a similar delusion with libertarians and their support of free markets.</a></li>
+<li><a href="https://posts.philipkd.com/the-problem-with-self-improvement-da6c303b6ed6">The Problem with Self-Improvement</a></li>
+<li><a href="https://posts.philipkd.com/the-wisdom-of-bored-inquiry-4e432d3ba46b">The Wisdom of Bored Inquiry</a></li>
+<li><a href="https://posts.philipkd.com/maybe-the-world-needs-a-narrow-state-d24140f8aa14">Maybe the World Needs a Narrow State</a></li>
+<li><a href="https://posts.philipkd.com/the-problem-with-our-understanding-of-ai-is-that-just-before-we-solve-a-challenge-like-mastering-aedd72d6a378">Hard vs Soft AI Problems</a>
+</ol>
+EOT;
+
+	}
+
+	function print_medium_plug() {
+
+		echo <<<EOT
+<h2><a href="https://medium.com/@philipkd/">New Essays on Medium</a></h2>
+EOT;
+
+	}
+
+	function print_contexts() {
+	    $tags = array_keys($GLOBALS['tag_to_essays']);
+	    usort($tags, "tag_count_sort");
+
+	    print "<h2>Special Tags</h2>";
+	    print '<div class="note-body">';
+
+	    foreach ($tags as $tag) {
+	    	if (special_tag($tag)) {
+		    	$count = count($GLOBALS['tag_to_essays'][$tag]);
+			    echo "<a href=\"?tag=$tag\">#$tag</a> " . ($count) . "<br/>\n";
+
+//			    echo "<a href=\"?tag=$tag\">" . capitalize_tag($tag) . "</a> ($count)<br/>\n";
+			}
+	    }
+
+	    print '</div>';
+
+	}
+
+	function print_essay($title) {
+
+		echo "<h4>" . ucfirst(titleify($title)) . "</h4>\n";		
+		echo "<div class=\"note-body\">";
+		echo MyMarkdown($GLOBALS['essays'][$title]);
+		echo "</div>\n\n";
+
+		$tags = $GLOBALS['essay_to_tags'][$title];
+
+		echo "<div class=\"note-tags\">";
+	    foreach ($tags as $tag) {
+	    	if (special_tag($tag) && $GLOBALS['local_access'] || !special_tag($tag))
+		    	echo "<img src=\"/tag.png\"> <a href='$tag'>" . $tag . "</a>\n";
+		}
+		echo "</div>";
+
+	}
+
+
+	function hr_tag() {
+		$spaces = str_repeat('&nbsp;',5);
+		$diamond = '&diams;';
+		print "<div class='hr'>" . $diamond . $spaces . $diamond . $spaces . $diamond . "</div>";
+	}
 
 	function tag_count_sort($a,$b) {
 		return count($GLOBALS['tag_to_essays'][$b]) - count($GLOBALS['tag_to_essays'][$a]);
@@ -194,6 +351,79 @@
 		return preg_match("/^_/",$tag);
 	}
 
+	function MyMarkDown($text) {
+		$text = Markdown($text);
+		// $text = preg_replace('/<blockquote>\s*?<p>/','<blockquote>',$text);
+		// $text = preg_replace('/<\/p>\s*?<\/blockquote>/','</blockquote>',$text);
+		return $text;
+	}
+
+
+?>
+
+<?php
+
+$tags = array_keys($GLOBALS['tag_to_essays']);
+foreach ($tags as $tag) {
+if (!special_tag($tag)) {
+$GLOBALS['tag_route'] = $tag;
+ob_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<title>Philosophistry: The Love of Rhetoric</title>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link rel="stylesheet" href="/db.css">
+
+</head>
+
+<body style="font-family: palatino; line-height: 135%;">
+
+<div id="page">
+
+<?php
+
 	print_nav();
 
+	print "<div id='content'>";
+
+	if ($GLOBALS['tag_route']) {
+ 		print_tag($GLOBALS['tag_route']);
+ 	} else {
+
+ 	// 	print_book();
+
+		// hr_tag();
+
+ 	// 	print_medium_plug();
+
+		// hr_tag();
+
+ 		print_tag('_new');
+
+ 		echo "<br/><br/><br/><br/>";
+ 	}
+
+ 	print "</div>";
+
+
+?>
+
+</div>
+
+</body>
+
+
+<?php
+file_put_contents(dirname(__FILE__) . "/../docs/db/$tag.html", ob_get_contents());
+ob_end_clean();
+print "$tag.html\n";
+# exit;
+}}
 ?>
