fced9306b1ab496b9d905dd55536f06dda796a66
Philip Dhingra <philipkd@gmail.com>
2018-11-21 07:30:55 -0600

    cleaned meta-analysis

--- scripts/build_wiki.php
+++ scripts/build_wiki.php
@@ -9,7 +9,7 @@ function slug($str) {
 }
 
 
-$folder = reldir() . '/../published/Wiki';
+$folder = reldir() . '/../content/live/Wiki';
 if ($folder_handle = opendir($folder)) {
 	while (false !== ($file = readdir($folder_handle))) {
 		if (preg_match('/\.txt$/',$file)) {
