5b152409c4cd20701947adeacd160ba5940db099
Philip Dhingra <philipkd@gmail.com>
2019-03-02 00:43:20 -0800

    entries are formatted roughly correctly in the wiki way

--- docs/index.php
+++ docs/index.php
@@ -208,12 +208,6 @@
  		
  		usort($essays,"essay_sort");
 
- 		$tag_name = $tag;
- 		if($GLOBALS['tag_to_name'][$tag])
- 			$tag_name = $GLOBALS['tag_to_name'][$tag];
-
-		echo "<h2>" . ucwords(tag_name_sub($tag)) . "</h2>\n";
-
 		$idea = ltrim($tag, "_");
 
 		if ($idea_data = $GLOBALS['index']->ideas->$idea) {
@@ -238,7 +232,7 @@
 			print_essay($essay);
 
 			// if($i < count($essays) - 1)
-			// 	hr_tag();
+			//  	hr_tag();
 			// $i++;
  		}
 	}
@@ -373,28 +367,43 @@ EOT;
 
 ?>
 
+<?
+
+
+	if ($tag = $GLOBALS['tag_route']) {
+		$tag_name = $tag;
+ 		if($GLOBALS['tag_to_name'][$tag])
+ 			$tag_name = $GLOBALS['tag_to_name'][$tag];
+
+		$title = ucwords(tag_name_sub($tag));
+	} else {
+		$title = "Philosophistry";
+	}
+
+
+?>
+
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<title>Philosophistry: The Love of Rhetoric</title>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="/db.css">
+<link rel="stylesheet" href="/wiki.css">
+
+<title><?= $title != "Philosophistry" ? "$title (Philosophistry)" : $title ?></title>
 
 </head>
 
-<body style="font-family: palatino; line-height: 135%;">
+<div class="site-title"><a href="/">Philosophistry</a> <a href="/">Database</a></div>
 
-<div id="page">
+<div class="entry">
+
+<div class="page-title"><?= $title ?></div>
 
 <?php
-
-
-	print "<div id='content'>";
 
 	if ($GLOBALS['tag_route']) {
  		print_tag($GLOBALS['tag_route']);
@@ -415,14 +424,7 @@ EOT;
 
  	}
 
- 	print '<br/><p/>
-<a href="https://licensebuttons.net/l/by/4.0/"><img src="https://licensebuttons.net/l/by/4.0/80x15.png"></a><br/><br/>';
-
- 	print "</div>";
-
-
 ?>
 
-</div>
-
-</body>
+<br/>
+<a href="https://licensebuttons.net/l/by/4.0/"><img src="https://licensebuttons.net/l/by/4.0/80x15.png"></a>
